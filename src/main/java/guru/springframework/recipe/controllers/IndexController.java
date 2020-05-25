d10754f44898866562f98f43a01a67647e1df32f
indranil1741997 <indranil1741997@gmail.com>
2020-05-25 20:19:34 +0530

    Inserting Data and Queries into database

--- src/main/java/guru/springframework/recipe/controllers/IndexController.java
+++ src/main/java/guru/springframework/recipe/controllers/IndexController.java
@@ -1,14 +1,34 @@
 package guru.springframework.recipe.controllers;
 
+import guru.springframework.recipe.domain.Category;
+import guru.springframework.recipe.domain.UnitOfMeasure;
+import guru.springframework.recipe.repositories.CategoryRepository;
+import guru.springframework.recipe.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 public class IndexController {
 
-    @RequestMapping({"","/","/index"})
-    public String getIndexPage(){
-        System.out.println("Hi!");
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
+    @RequestMapping({"", "/", "/index"})
+    public String getIndexPage() {
+
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("TeaSpoon");
+
+        System.out.println("Cat Id is:" + categoryOptional.get().getId());
+        System.out.println("UOM Id is:" + unitOfMeasureOptional.get().getId());
+
         return "index";
     }
 }
