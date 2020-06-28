0d9302c184ec93a375015f6d4330b3df04be3f75
indranil1741997 <indranil1741997@gmail.com>
2020-06-28 20:13:00 +0530

    Adding Unit Test for IndexController

---
+++ src/test/java/guru/springframework/recipe/controllers/IndexControllerTest.java
@@ -0,0 +1,37 @@
+package guru.springframework.recipe.controllers;
+
+import guru.springframework.recipe.services.RecipeService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.ui.Model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+class IndexControllerTest {
+
+    @Mock
+    RecipeService recipeService;
+
+    @Mock
+    Model model;
+
+    IndexController indexController;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+
+        indexController = new IndexController(recipeService);
+    }
+
+    @Test
+    void getIndexPage() {
+        String viewName = indexController.getIndexPage(model);
+        assertEquals("index", viewName);
+        verify(recipeService, times(1)).getRecipes();
+        verify(model, times(1)).addAttribute(eq("recipes"), anySet());
+    }
+}
