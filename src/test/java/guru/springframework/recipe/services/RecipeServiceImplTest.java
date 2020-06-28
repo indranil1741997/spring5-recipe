b1514552de1e978e7dcbf6ec597d33ed50d85e22
indranil1741997 <indranil1741997@gmail.com>
2020-06-28 14:23:10 +0530

    Using Mock Repository in JUnit Tests

---
+++ src/test/java/guru/springframework/recipe/services/RecipeServiceImplTest.java
@@ -0,0 +1,43 @@
+package guru.springframework.recipe.services;
+
+import guru.springframework.recipe.domain.Recipe;
+import guru.springframework.recipe.repositories.RecipeRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+class RecipeServiceImplTest {
+
+    RecipeServiceImpl recipeService;
+
+    @Mock
+    RecipeRepository recipeRepository;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+
+        recipeService = new RecipeServiceImpl(recipeRepository);
+    }
+
+    @Test
+    void getRecipes() {
+
+        Recipe recipe = new Recipe();
+        HashSet recipesData = new HashSet();
+        recipesData.add(recipe);
+
+        when(recipeRepository.findAll()).thenReturn(recipesData);
+
+        Set<Recipe> recipeSet = recipeService.getRecipes();
+        assertEquals(recipeSet.size(), 1);
+        verify(recipeRepository, times(1)).findAll();
+    }
+}
